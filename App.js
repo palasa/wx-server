bba4c1a61876393841ccf880d51f5d8e6903efb6
palasa <170394205@qq.com>
2022-04-09 21:38:19 +0800

    update

---
+++ App.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const { init: initDB, Counter } = require("./db");
+
+const logger = morgan("tiny");
+
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+app.use(logger);
+
+// 首页
+app.get("/", async (req, res) => {
+    res.sendFile(path.join(__dirname, "app.html"));
+});
+
+// 小程序调用，获取微信 Open ID
+app.get("/api/wx_openid", async (req, res) => {
+    if (req.headers["x-wx-source"]) {
+      res.send(req.headers["x-wx-openid"]);
+    }
+});
+
+const port = process.env.PORT || 80;
+
+async function bootstrap() {
+  await initDB();
+  app.listen(port, () => {
+    console.log("启动成功", port);
+  });
+}
+
+bootstrap();
